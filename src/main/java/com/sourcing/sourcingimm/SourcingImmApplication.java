102df30131e058e9932f54c56e5879f7d8897472
logan496 <fils.tsajio@2027.ucac-icam.com>
2025-05-27 08:44:46 +0100

    initial commit

---
+++ src/main/java/com/sourcing/sourcingimm/SourcingImmApplication.java
@@ -0,0 +1,13 @@
+package com.sourcing.sourcingimm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SourcingImmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SourcingImmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sourcing/sourcingimm/SourcingImmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sourcing.sourcingimm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SourcingImmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
